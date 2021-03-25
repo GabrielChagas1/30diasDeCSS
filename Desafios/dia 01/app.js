35027de7e755180f780307b12d4fe14d079f037b
Gabriel Chagas <gabrielserq@outlook.com>
2021-03-25 00:23:24 -0300

    add arquivos

---
+++ Desafios/dia 01/app.js
@@ -0,0 +1,22 @@
+let $ = document.querySelector.bind(document);
+
+function orangeColor(){
+    document.documentElement.style.setProperty('--color-primary', '#F0841D');
+    document.documentElement.style.setProperty('--color-secondary', '#ffc048');
+}
+
+function platinumColor(){
+    document.documentElement.style.setProperty('--color-primary', '#989ca1');
+    document.documentElement.style.setProperty('--color-secondary', '#8395a7');
+}
+
+function blackColor(){
+    document.documentElement.style.setProperty('--color-primary', '#282929');
+    document.documentElement.style.setProperty('--color-secondary', '#535c68');
+}
+
+function businessColor(){
+    document.documentElement.style.setProperty('--color-primary', '#027a75');
+    document.documentElement.style.setProperty('--color-secondary', '#535c68');
+}
+
