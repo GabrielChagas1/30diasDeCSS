8e6ec0c104cbbab6c8c2e078bf7543a7be6b4877
Gabriel Chagas <gabrielserq@outlook.com>
2022-02-24 17:11:59 -0300

    chore: creating js show modal

--- Desafios/dia 38/assets/js/main.js
+++ Desafios/dia 38/assets/js/main.js
@@ -1,4 +1,15 @@
 /*=============== SHOW MODAL ===============*/
+const showModal = (openButton, modalContent) => {
+    const openBtn = document.getElementById(openButton),
+    modalContainer = document.getElementById(modalContent)
 
+    if(openBtn && modalContainer){
+        openBtn.addEventListener('click', () => {
+            modalContainer.classList.add('show-modal')
+        })
+    }
+}
+
+showModal('open-modal', 'modal-container');
 
 /*=============== CLOSE MODAL ===============*/
